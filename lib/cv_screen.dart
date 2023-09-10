42bafb05177f2305518595f79315464d2ae718cd
Wilson Aballey <willywallet001@gmail.com>
2023-09-10 22:40:32 +0000

    Second Commit

---
+++ lib/cv_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CVScreen extends StatefulWidget {
+  const CVScreen({super.key});
+
+  @override
+  State<CVScreen> createState() => _CVScreenState();
+}
+
+class _CVScreenState extends State<CVScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
