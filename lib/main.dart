f993f5c180c4a85f3ae5e145957af72176321a30
Wilson Aballey <willywallet001@gmail.com>
2023-09-11 09:20:50 +0000

    Welcome Screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_cv_application/cv_screen.dart';
+import 'package:mobile_cv_application/welcome_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: CVScreen(),
+      home: WelcomeScreen(),
     );
   }
 }
---
+++ lib/welcome_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_cv_application/cv_screen.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            constraints: BoxConstraints(maxHeight: 1000, maxWidth: 1000),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/background.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/logo.jpg",
+                  width: 150,
+                  height: 150,
+                ),
+                const SizedBox(height: 20),
+
+                const Text(
+                  "Curriculum Vitae",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                // Welcome message
+                const Text(
+                  "Access my CV by clicking on the View button",
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 10),
+
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigate to your main screen
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (context) => CVScreen(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "View",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                const SizedBox(
+                  height: 70,
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
